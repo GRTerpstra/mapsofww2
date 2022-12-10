ba93fa58fdf571363906294cc2855a9636902e36
Gerwin Terpstra <gerwin_terpstra@live.nl>
2022-12-10 22:09:23 +0100

    WIP: Working on documentsMock json

--- src/components/documents/DocumentImages.tsx
+++ src/components/documents/DocumentImages.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react"
+
 const DocumentImages = (props: any) => {
+
+    const [state, setState] = useState({
+        loadingIcon: true,
+    })
+
+    function setLoadingIconFalse() {
+        console.log('setFalse')
+        setState((prevState) => ({
+            ...prevState,
+            loadingIcon: false
+        }))
+    }
 
     const imageElements: any = props.images.map((imageURL: string) => (
         <div className="document-images__image-wrapper">
-            <img className="document-images__image" src={imageURL} />
+            <img className="document-images__image" onLoad={setLoadingIconFalse} src={imageURL} />
         </div>
     ))
 
     return (
         <div className="document-images">
+            {state.loadingIcon && <div className="document-images__loader-icon-container">
+                <div className="document-images__loader-icon-background" />
+                <img className="document-images__loader-icon" id="loaderIcon" src="icons/loader-icon.svg" alt="Loading map..." />
+            </div>}
             <div className="document-images__wrapper">
                 <div className="document-images__container">
                     {imageElements}
--- src/components/documents/DocumentVideos.tsx
+++ src/components/documents/DocumentVideos.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
+
 const DocumentVideos = (props: any) => {
 
+    const [state, setState] = useState({
+        loadingIcon: true,
+    })
+
+    function setLoadingIconFalse() {
+        console.log('setFalse')
+        setState((prevState) => ({
+            ...prevState,
+            loadingIcon: false
+        }))
+    }
+
     const videoElements: any = props.youtubeVideos.map((videoURL: string) => (
-        <iframe className="document-videos__video" src={videoURL} title="World War II in Europe with Flags: Every Day" allow="accelerometer clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe className="document-videos__video" src={videoURL} title="Video" onLoad={() => setLoadingIconFalse()} allow="accelerometer clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     ))
 
     return (
         <div className="document-videos">
+            {state.loadingIcon && <div className="document-videos__loader-icon-container">
+                <div className="document-videos__loader-icon-background" />
+                <img className="document-videos__loader-icon" id="loaderIcon" src="icons/loader-icon.svg" alt="Loading map..." />
+            </div>}
             <div className="document-videos__wrapper">
                 <div className="document-videos__container">
                     {videoElements}
